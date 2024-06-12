6f36e03f7cc951cdb1c7a58386f2b5b0cc8650e4
LuisGomez-Meneses <luismiguelgomez0821@gmail.com>
2024-06-12 18:37:10 -0500

    practica74

---
+++ src/analysis/scatter_plots_feature_vs_centroid_y.py
@@ -0,0 +1,66 @@
+"""
+Author: Luis Miguel Gómez Meneses
+Email: luisgomez251811@correo.itm.edu.co
+Date: 12/06/2024
+Version: 1.0
+
+Description:
+This script reads a CSV file, selects specific columns, and creates scatter plots of these features against the 'centroid_y' column. The plots are displayed and can be saved as image files.
+
+Dependencies:
+- pandas (version 2.0.3)  # Update with the correct version
+- matplotlib (version 3.4.2)  # Update with the correct version
+- Python (version  3.10.12)  # Update with the correct version
+
+Usage:
+Example of how to use the function in this file:
+>>> import pandas as pd
+>>> import matplotlib.pyplot as plt
+>>> path = 'Cyclist-Detection/data/processed/cyclist_detection_Normalized_data.csv'
+>>> df = pd.read_csv(path)
+>>> from scatter_plots_feature_vs_centroid_y import scatter_plots_feature_vs_centroid_y
+>>> scatter_plots_feature_vs_centroid_y(df)
+
+Additional Information:
+- Make sure to have the dependencies installed before running the code.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path = 'Cyclist-Detection/data/processed/cyclist_detection_Normalized_data.csv'
+df = pd.read_csv(path)
+selected_columns = ['avg_angle', 'avg_distance', 'texture', 'contrast']
+
+def scatter_plots_feature_vs_centroid_y(df):
+    """
+    Creates scatter plots of selected features vs 'centroid_y' and saves the plots as an image file.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame containing the data.
+
+    Returns:
+    None
+    """
+    # Convert 'centroid_y' column to numeric type
+    df['centroid_y'] = pd.to_numeric(df['centroid_y'])
+
+    # Set up the figure and axes for the scatter plots
+    plt.figure(figsize=(12, 8))
+
+    # Loop through selected columns and create scatter plots
+    for i, feature in enumerate(selected_columns):
+        plt.subplot(2, 2, i + 1)
+        plt.scatter(df[feature], df['centroid_y'], alpha=0.5)
+        plt.title(f'Scatter Plot of {feature} vs centroid_y')
+        plt.xlabel(feature)
+        plt.ylabel('centroid_y')
+        plt.ylim(df['centroid_y'].min() - 0.1, df['centroid_y'].max() + 0.1)
+
+    # Adjust layout and save the figure as a PNG file
+    plt.tight_layout()
+    plt.savefig('results/figure/scatter plots/scatter_plots_feature_vs_centroid_y.png', format='png', dpi=300)
+    plt.show()
+
+# Uncomment to generate and save the scatter plots as an image file
+scatter_plots_feature_vs_centroid_y(df)
